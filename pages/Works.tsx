db2b4d92050216d83e02be5d69d3730b2013e739
unknown <Fal-hi>
2023-06-18 16:23:58 +0700

    penambahan isi halaman works

--- pages/Works.tsx
+++ pages/Works.tsx
@@ -1,19 +1,21 @@
-import Image, { StaticImageData } from 'next/image';
-import Web1 from '@/public/images/web1.jpg';
-import Web2 from '@/public/images/web2.jpg';
-import Web3 from '@/public/images/web3.jpg';
-import Web4 from '@/public/images/web4.jpg';
-import Web5 from '@/public/images/web5.jpg';
-import { Carousel } from 'react-responsive-carousel';
-import 'react-responsive-carousel/lib/styles/carousel.min.css';
-import { useEffect, useRef, useState } from 'react';
-import { LeftArrow, RightArrow, X } from '@/components/icons';
-import Link from 'next/link';
+import Image, { StaticImageData } from "next/image";
+import Web1 from "@/public/images/book-search.png";
+import Web2 from "@/public/images/guessing-game.png";
+import Web3 from "@/public/images/web3.jpg";
+import Web4 from "@/public/images/web4.jpg";
+import Web5 from "@/public/images/web5.jpg";
+import Web6 from "@/public/images/web1.jpg";
+import { Carousel } from "react-responsive-carousel";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { useEffect, useRef, useState } from "react";
+import { LeftArrow, RightArrow, X } from "@/components/icons";
+import Link from "next/link";
 
 interface ImageList {
   id: number;
   name: StaticImageData;
   alt: string;
+  tech: string;
   link: string;
 }
 
@@ -21,62 +23,74 @@ const imageLists: ImageList[] = [
   {
     id: 1,
     name: Web1,
-    alt: 'Nest JS CRUD',
-    link: "https://github.com/Fal-hi/NestJS-CRUD"
+    alt: "Book Search",
+    tech: "Next JS, Tailwind CSS",
+    link: "https://book-search-fal.netlify.app",
   },
   {
     id: 2,
     name: Web2,
-    alt: 'Express JS CRUD',
-    link: "https://github.com/Fal-hi/ExpressJS-CRUD"
+    alt: "Guessing Game",
+    tech: "Next JS, Tailwind CSS",
+    link: "https://guessing-game-fal.vercel.app",
   },
   {
     id: 3,
     name: Web3,
-    alt: 'React Dashboard Redux Saga',
-    link: "https://github.com/Fal-hi/react-dashboard-redux-saga"
+    alt: "Nest JS CRUD",
+    tech: "Nest JS, PostgreSQL, Tailwind CSS",
+    link: "https://github.com/Fal-hi/NestJS-CRUD",
   },
   {
     id: 4,
     name: Web4,
-    alt: 'React Portfolio',
-    link: "https://github.com/Fal-hi/react-portfolio"
+    alt: "Express JS CRUD",
+    tech: "Express JS, PostgreSQL, Tailwind CSS",
+    link: "https://github.com/Fal-hi/ExpressJS-CRUD",
   },
   {
     id: 5,
     name: Web5,
-    alt: 'UMKM Website',
-    link: "https://github.com/Fal-hi/umkm"
+    alt: "React Dashboard Redux Saga",
+    tech: "React JS, Redux Saga, PostgreSQL, Tailwind CSS",
+    link: "https://github.com/Fal-hi/react-dashboard-redux-saga",
+  },
+  {
+    id: 6,
+    name: Web6,
+    alt: "UMKM Website",
+    tech: "PHP, MySQL, Bootstrap CSS",
+    link: "https://github.com/Fal-hi/umkm",
   },
 ];
 
 export default function Works() {
   const [showModal, setShowModal] = useState<boolean>(false);
-  const [modalImage, setModalImage] = useState<any>('');
-  const [modalImageName, setModalImageName] = useState<string>('');
-  const [modalImageLink, setModalImageLink] = useState<string>('');
+  const [modalImage, setModalImage] = useState<any>("");
+  const [modalImageName, setModalImageName] = useState<string>("");
+  const [modalImageLink, setModalImageLink] = useState<string>("");
   const [currentIndex, setCurrentIndex] = useState<number>(0);
 
-  const ref = useRef<HTMLDivElement>(null)
+  const ref = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
     const handleClickOutside = (e: MouseEvent) => {
-      if(showModal && ref.current && !ref.current.contains(e.target as Node)) {
-        setShowModal(false)
+      if (showModal && ref.current && !ref.current.contains(e.target as Node)) {
+        setShowModal(false);
       }
-    }
-    document.addEventListener('mousedown', handleClickOutside)
+    };
+    document.addEventListener("mousedown", handleClickOutside);
     return () => {
-      document.removeEventListener('mousedown', handleClickOutside)
-    }
-  }, [showModal])
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [showModal]);
 
   const openModal = (src: any, alt: string, link: string) => {
-    setModalImage(src)
-    setModalImageName(alt)
-    setModalImageLink(link)
+    setModalImage(src);
+    setModalImageName(alt);
+    setModalImageLink(link);
     setShowModal(true);
-    setCurrentIndex(imageLists.findIndex((image) => image.name.src === src))
+    setCurrentIndex(imageLists.findIndex((image) => image.name.src === src));
     // document.body.classList.add('modal-open');
   };
 
@@ -87,11 +101,19 @@ export default function Works() {
 
   const handlePrev = () => {
     setCurrentIndex((currentIndex - 1 + imageLists.length) % imageLists.length);
-    setModalImage(imageLists[(currentIndex - 1 + imageLists.length) % imageLists.length].name);
-    setModalImageName(imageLists[(currentIndex - 1 + imageLists.length) % imageLists.length].alt);
-    setModalImageLink(imageLists[(currentIndex - 1 + imageLists.length) % imageLists.length].link);
+    setModalImage(
+      imageLists[(currentIndex - 1 + imageLists.length) % imageLists.length]
+        .name
+    );
+    setModalImageName(
+      imageLists[(currentIndex - 1 + imageLists.length) % imageLists.length].alt
+    );
+    setModalImageLink(
+      imageLists[(currentIndex - 1 + imageLists.length) % imageLists.length]
+        .link
+    );
   };
-  
+
   const handleNext = () => {
     setCurrentIndex((currentIndex + 1) % imageLists.length);
     setModalImage(imageLists[(currentIndex + 1) % imageLists.length].name);
@@ -138,9 +160,10 @@ export default function Works() {
                 className="object-cover object-center h-[330px]"
               />
               <div className="font-sans absolute top-0 bottom-0 left-0 right-0 py-2 w-full mx-auto shadow-sm bg-black bg-opacity-50 text-white opacity-0 hover:opacity-100 transition-opacity duration-300">
-                <p className="text-center mt-[20vh] md:mt-[12.5vh] lg:mt-[20vh]">
+                <p className="text-center font-semibold mt-[20vh] md:mt-[12.5vh] lg:mt-[20vh]">
                   {image.alt}
                 </p>
+                <p className="text-xs">{image.tech}</p>
               </div>
             </div>
           ))}
